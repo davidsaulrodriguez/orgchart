97d7ca44605701fa1f154ad96402a5124b42a5f5
David Rodriguez <david@bsdadm.com>
2021-03-15 20:03:04 -0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Typography } from "@material-ui/core";
+
+function App() {
+  return (
+    <>
+      <Typography variant='h1' align='center'>
+        OrgChart
+      </Typography>
+      <Typography variant='subtitle1' align='center'>
+        ...currently in development...
+      </Typography>
+    </>
+  );
+}
+
+export default App;
