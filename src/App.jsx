be7a4ee765c178f814ec48d20ed5e6d87faa3da2
David Rodriguez <david@bsdadm.com>
2021-03-20 23:02:22 -0400

    modified to build the ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import "./App.css";
-import { Typography } from "@material-ui/core";
+import React from 'react';
+import './App.css';
+import { CssBaseline, Container } from '@material-ui/core';
+import Nav from './components/Nav/Nav';
+import Home from './pages/Home';
 
-function App() {
+const App = () => {
   return (
     <>
-      <Typography variant='h1' align='center'>
-        OrgChart
-      </Typography>
-      <Typography variant='subtitle1' align='center'>
-        ...currently in development...
-      </Typography>
+      <CssBaseline />
+      <Nav />
+      <Container>
+        <Home />
+      </Container>
     </>
   );
-}
+};
 
 export default App;
